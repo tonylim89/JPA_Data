e082f2bca525da41e14e80eca542322aadc6141b
tonylim89 <tonylim9@hotmail.com>
2023-06-11 19:51:01 +0800

    re-adding in initialization

---
+++ src/main/java/sg/edu/nus/iss/data_jpa_worshop/DataJpaWorshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.data_jpa_worshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaWorshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaWorshopApplication.class, args);
+	}
+
+}
