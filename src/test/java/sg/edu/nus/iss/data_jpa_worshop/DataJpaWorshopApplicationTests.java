3e39f291d43cc4763a06019e4ce6621f8a8de592
tonylim89 <tonylim9@hotmail.com>
2023-06-11 19:23:39 +0800

    initial springboot upload

---
+++ src/test/java/sg/edu/nus/iss/data_jpa_worshop/DataJpaWorshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.data_jpa_worshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaWorshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
